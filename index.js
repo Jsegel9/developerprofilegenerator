53b6acfbeb9b8d5d1b6615f59ab503a54b22538e
Jsegel <segeljosh@gmail.com>
2020-01-11 14:22:53 -0600

    adding inquirer and fs

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 var fs = require("fs");
 var inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Question 1",
+            name: "q1"
+        }
+    ])
+    .then(function(response){
+        console.log(response)
+        console.log(response.q1)
+    })
