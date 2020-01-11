a177218a2b913d0af32def273510953f60ac02e2
Jsegel <segeljosh@gmail.com>
2020-01-11 15:44:17 -0600

    adding functionality to write to html

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ inquirer
         }
     ])
     .then(function(response){
-        console.log(response)
-        console.log(response.q1)
+        console.log(response);
+        console.log(response.q1);
+        const $html = `<div>${response.q1}</div>`
+        fs.writeFile("index.html", $html, function(err){
+            if (err) throw (err)
+        })
     })
