0c8b7ab99e18f7aa8ee6b7cba6bdf212bda0f5e5
Jsegel <segeljosh@gmail.com>
2020-01-12 21:42:32 -0600

    adding jquery

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 var fs = require("fs");
 var inquirer = require("inquirer");
+var $ = require("jquery");
 
 inquirer
     .prompt([
         {
             type: "input",
-            message: "Question 1",
+            message: "What is your name?",
             name: "q1"
+        },
+        {
+            type: "input",
+            message: "What is your Github username?",
+            name: "username"
         }
     ])
     .then(function(response){
-        console.log(response);
-        console.log(response.q1);
-        const $html = `<div>${response.q1}</div>`
+        $.ajax({
+            url: "https://api.github.com/users/" + response.username,
+            method: "GET"
+        }).then(function(res){
+            console.log(res);
+        })
+        const $html = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Developer Profile</title>
+        </head>
+        <body>
+                <header>
+                    <div class="imgcontain">
+                       <img src="">
+                    </div>
+                    <h1>Hello</h1>
+                    <h2>My name is ${response.q1}</h2>
+                </header>
+        </body>
+        </html>`
         fs.writeFile("index.html", $html, function(err){
             if (err) throw (err)
         })
